decb85830929b98198bd4f14071629c386e11c97
namiko <namiko@posteo.de>
2021-11-06 16:15:35 +0100

    testing scripting api

---
+++ scripts/index.ts
@@ -0,0 +1,12 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+
+// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+bootstrapExtra().catch(e => console.error(e));
+
+var currentPopup: any = undefined;
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes();
+
+
