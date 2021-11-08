cc0277b84c64ce00339556291846bd6897a397a1
namiko <namiko@posteo.de>
2021-11-08 19:56:06 +0100

    removed .tsx-files

--- tiles_png/Iglu/Tuxe.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Tuxe" tilewidth="32" tileheight="32" tilecount="96" columns="12">
- <image source="Tuxe.png" width="401" height="263"/>
-</tileset>
--- tiles_png/Iglu/Wand_Iglu.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Wand_Iglu" tilewidth="32" tileheight="32" tilecount="30" columns="6">
- <image source="Wand_Iglu.png" width="203" height="179"/>
-</tileset>
